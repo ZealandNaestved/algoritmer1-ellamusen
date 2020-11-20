a071f171ebc09a52639590f7c343b01735b74479
ellamusen <dani_1997@live.dk>
2020-11-20 11:45:20 +0100

    Initial commit

--- src/Algorithms2.java
+++ src/Algorithms2.java
@@ -5,11 +5,23 @@
  * 27/10/2020
  */
 
-public class    Algorithms2 {
+public class Algorithms2 {
 
     // TODO 1 Skriv en metode her i denne klasse, som kan finde et tal i Data klassens randomNumbers1000 felt med LINEAR SEARCH
 
+    public int linearsearch(int[] haystack, int needle) {  // variable names for better remembering
+        for (int index = 0; index < haystack.length; index++) {  // i is named index for better  remembering
+            //  If the needle exists at the current index in the haystack (array), return that index
+            if (haystack[index] == needle) {
+                return index;
+            }
+        }
+        return -1; // return is placed outside the for loop, because if placed in loop, the loop will not run till the end
+    }
+
     // TODO 2 Skriv en metode her i denne klasse, som kan finde et navn i Data klassens randomDrengeNavne felt med LINEAR SEARCH
+
+
 
     // TODO 3 Skriv en metode her i denne klasse, som kan returnere et sorteret array fra randomNumbers1000
 
